c65f7cec9b23207de390697f48eccb93d561bfc2
ASUS <erinaakter883@gmail.com>
2023-03-19 16:32:18 +0600

    Custom Recycler View

--- app/src/main/java/com/erina/mypractice/NameViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.erina.mypractice
-
-import androidx.recyclerview.widget.RecyclerView
-import com.erina.mypractice.databinding.ItemNameBinding
-
-
-class NameViewHolder(val binding: ItemNameBinding): RecyclerView.ViewHolder(binding.root) {
-
-}
